873e41ab401ed288a0b610d5922ed85bb106fb21
omega5300 <omegamatrix5351@gmail.com>
2020-11-24 20:51:10 -0500

    api

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 
 /** @type {number}  */
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 
 // initial
 const app = express();
